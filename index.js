e62394a373fa68ee3fcb1a4037665b99a036e6e2
Bittu-26 <ayushrawat4594@gmail.com>
2025-11-09 22:43:42 +0530

    new

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ import random from "random";
 
 const path = "./data.json";
 
+const markCommit = (x, y) => {
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = {
+    date: date,
+  };
+
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date }).push();
+  });
+};
+
 const makeCommits = (n) => {
   if(n===0) return simpleGit().push();
   const x = random.int(0, 54);
